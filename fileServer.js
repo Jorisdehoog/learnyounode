6dc178782da6a97984882c16a3574c705a35e971
Joris <dehoogjoris@gmail.com>
2018-10-20 17:51:33 +0200

    I need to stream a file to the requestor. Made a readableStream, I can read it in the correct order, but I need to stream it to the request, and not sure yet how to do it

---
+++ fileServer.js
@@ -0,0 +1,33 @@
+var http = require('http')
+var fs = require('fs')
+
+var port = process.argv[2];
+var file = process.argv[3];
+
+var server = http.createServer(function(req, res){
+    // request handling logic
+    console.log(port);
+    console.log(req.rawHeaders);
+    console.log('Fetch this: ' + file);
+    
+    // represent the requested file as a node stream
+    const found = fs.createReadStream(file);
+    found.setEncoding('utf8')
+    output = '';
+    found.on('data', function(chunk){
+        // console.log(chunk);
+        output += chunk;
+    })
+    found.on('end', function(){
+        console.log(output)
+        found.pipe(res);
+    })
+
+    found.on('close', function(){
+        // stop server
+        res.end();
+    })
+    // found.pipe(res);
+    
+})
+server.listen(port)
